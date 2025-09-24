bfeea7b929998d593a104ac28d10b9f5980f592d
AJABHIMANYU <63732138+AJABHIMANYU@users.noreply.github.com>
2025-09-24 14:51:38 +0530

    Create deploy.sql

---
+++ deploy.sql
@@ -0,0 +1,6 @@
+-- Deploy all database objects
+@../ddl/tables.sql
+@../packages/simple_employee_mgmt_pkg.pks
+@../packages/simple_employee_mgmt_pkg.pkb
+@../procedures/update_dept_salary.prc
+@../functions/get_dept_count.fnc
